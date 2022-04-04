e4f80ad8fe1397b809a23087673de0da6a67c67c
Riajul Islam <riajulpislam@gmail.com>
2022-04-04 13:20:58 +0600

    js dom

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let title = document.getElementById('title')
-console.log(title)
-let pavel = document.getElementsByClassName('lead')
-console.log(pavel)
-let  list = document.getElementsByTagName('li')
-console.log(list)
+
+let box = document.querySelectorAll('.box:not(#box3)')
+console.log(box)
+let box2 = document.querySelectorAll('.box:not(#box2)')
+console.log(box2)
