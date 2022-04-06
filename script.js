bb8191208c1f9d32ce56a1934b57f22ecc858f32
Riajul Islam <riajulpislam@gmail.com>
2022-04-06 16:52:13 +0600

    button event

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
-/*function makeRed(){
-    document.body.style.backgroundColor = 'red'
+/*function handle(){
+    const click = document.getElementById('clik-method')
+    click.innerText = 'riajul islam pavel'
 }
-const blue = document.getElementById('blue-button')
-blue.onclick = pavel
-function pavel(){
-    document.body.style.backgroundColor = 'blue'
-} 
-const pink = document.getElementById('pink-button')
-pink.onclick = function(){
-    document.body.style.backgroundColor = '#00cec9'
-}
-const black = document.getElementById('makeblack')
-black.addEventListener('click', make)
-function make(){
-    document.body.style.backgroundColor = '#fab1a0'
-}
-const me = document.getElementById('makeMe')
-me.addEventListener('click',function make(){
-    document.body.style.backgroundColor = '#e84393'
-})*/
-function makeRed(){
-    document.body.style.backgroundColor = 'red'
-}
-const blue = document.getElementById('makeblue')
-blue.onclick = pavel
-function pavel(){
-    document.body.style.backgroundColor = 'blue'
-}
-const pink = document.getElementById('makepink') 
-pink.onclick = function(){
-    document.body.style.backgroundColor = '#00cec9'
-}
-const black = document.getElementById('makeblack')
-black.addEventListener('click',function make(){
-    document.body.style.backgroundColor = '#636e72'
+document.getElementById('event-click').addEventListener('click',function(){
+    const clik = document.getElementById('clik-method')
+    clik.innerText = 'tamirul millat kamiil madrash'
 })
-document.getElementById('makegray').addEventListener('click',function(){
-    document.body.style.backgroundColor = '#b2bec3'
+document.getElementById('update').addEventListener('click',function(){
+    const floors = document.getElementById('field')
+    const click = document.getElementById('clik-method')
+    click.innerText = click.value
+})*/
+function method() {
+const here = document.getElementById('function')
+here.innerText = 'Riajul islam pavel'
+}
+document.getElementById('handle').addEventListener('click',function(){
+  const ppp = document.getElementById('pavel')
+  ppp.innerText = 'tamirul millat kamill madarsh'
 })
