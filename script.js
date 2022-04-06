b01b1fc97da6cc6b06b64316975567bafd3af036
Riajul Islam <riajulpislam@gmail.com>
2022-04-06 15:10:26 +0600

    dom event

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-function makemore(){
-    document.body.style.backgroundColor = 'black';
+/*function makeRed(){
+    document.body.style.backgroundColor = 'red'
 }
-const orange = document.getElementById('orange-button')
-orange.onclick = pavel
+const blue = document.getElementById('blue-button')
+blue.onclick = pavel
 function pavel(){
-    document.body.style.backgroundColor = 'orange'
+    document.body.style.backgroundColor = 'blue'
+} 
+const pink = document.getElementById('pink-button')
+pink.onclick = function(){
+    document.body.style.backgroundColor = '#00cec9'
 }
-const greenButton = document.getElementById('green-button')
-greenButton.onclick = function makeGreen(){
-    document.body.style.backgroundColor ='green'
+const black = document.getElementById('makeblack')
+black.addEventListener('click', make)
+function make(){
+    document.body.style.backgroundColor = '#fab1a0'
 }
+const me = document.getElementById('makeMe')
+me.addEventListener('click',function make(){
+    document.body.style.backgroundColor = '#e84393'
+})*/
+function makeRed(){
+    document.body.style.backgroundColor = 'red'
+}
+const blue = document.getElementById('makeblue')
+blue.onclick = pavel
+function pavel(){
+    document.body.style.backgroundColor = 'blue'
+}
+const pink = document.getElementById('makepink') 
+pink.onclick = function(){
+    document.body.style.backgroundColor = '#00cec9'
+}
+const black = document.getElementById('makeblack')
+black.addEventListener('click',function make(){
+    document.body.style.backgroundColor = '#636e72'
+})
+document.getElementById('makegray').addEventListener('click',function(){
+    document.body.style.backgroundColor = '#b2bec3'
+})
