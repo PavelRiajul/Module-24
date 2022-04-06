ba47206c07d0e7ec3d7a76938489162cbb4a8c7f
Riajul Islam <riajulpislam@gmail.com>
2022-04-07 00:43:28 +0600

    login form

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-/*function handle(){
-    const click = document.getElementById('clik-method')
-    click.innerText = 'riajul islam pavel'
-}
-document.getElementById('event-click').addEventListener('click',function(){
-    const clik = document.getElementById('clik-method')
-    clik.innerText = 'tamirul millat kamiil madrash'
-})
-document.getElementById('update').addEventListener('click',function(){
-    const floors = document.getElementById('field')
-    const click = document.getElementById('clik-method')
-    click.innerText = click.value
-})*/
-function method() {
-const here = document.getElementById('function')
-here.innerText = 'Riajul islam pavel'
-}
-document.getElementById('handle').addEventListener('click',function(){
-  const ppp = document.getElementById('pavel')
-  ppp.innerText = 'tamirul millat kamill madarsh'
+document.getElementById('login-submit').addEventListener('click', function (){
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value
+    console.log(userEmail)
+    //password
+    const passwordField = document.getElementById('user-password')
+    const userPasswoed =passwordField.value
+    console.log(userPasswoed)
 })
